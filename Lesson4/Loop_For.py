62d77bb531a807f3b555aaa2bf8c014720bb523a
TrujoDev <andetox@gmail.com>
2024-03-25 22:37:28 -0600

    commit-loop-for

---
+++ Lesson4/Loop_For.py
@@ -0,0 +1,78 @@
+lista:list[str] = ['a','b','c','d']
+
+for letra in lista:
+    numero_letra = lista.index(letra)+1
+    print(f"Letra {numero_letra}: {letra}")
+
+
+lista:list[str] = ['pablo','laura','fede','luis','julia']
+for nombre in lista:
+    if nombre.startswith('l'):
+        print(nombre)
+    else:
+        print('nombre que no empieza con L')
+
+
+numeros:list[int] = [1,2,3,4,5]
+mi_valor = 0
+for numero in numeros:
+    mi_valor = mi_valor + numero
+print(mi_valor)
+
+
+palabra:str = 'python'
+for letra in palabra:
+    print(letra)
+
+
+dic = {'clave1':'a','clave2':'b','clave3':'c'}
+for a,b in dic.items():
+    print(a,b)
+    
+# Práctica Loop For 1
+
+# Utilizando loops For, saluda a todos los miembros de una clase, imprimiendo "Hola" + su nombre.
+
+# Por ejemplo: "Hola María"
+
+alumnos_clase = ["María", "José", "Carlos", "Martina", "Isabel", "Tomás", "Daniela"] 
+
+for item in alumnos_clase:
+    print("Hola " + item)
+
+# Práctica Loop For 2
+
+# Dada la siguiente lista de números, realiza la suma de todos los números utilizando loops For y almacena el resultado de la suma en una variable llamada suma_numeros:
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_numeros = 0
+
+for numero in lista_numeros:
+    suma_numeros += numero
+
+print(suma_numeros)
+
+# Práctica Loop For 3
+
+# Dada la siguiente lista de números, realiza la suma de todos los números pares e impares* por separado en las variables suma_pares y suma_impares respectivamente:
+
+# lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+
+# *Recordando de los días anteriores: el módulo (o resto) de un número dividido 2 es cero cuando dicho valor es par, y 1 cuando es impar
+
+# num % 2 == 0 (valores pares)
+
+# num % 2 == 1 (valores impares)
+
+lista_numeros = [1,5,8,7,6,8,2,5,2,6,4,8,5,9,8,3,5,4,2,5,6,4]
+suma_pares = 0
+suma_impares = 0
+
+for numero in lista_numeros:
+    if  numero % 2 == 0:
+        suma_pares += numero
+    else:
+        suma_impares += numero
+
+print(suma_pares)
+print(suma_impares)
