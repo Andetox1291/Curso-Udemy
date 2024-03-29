40e0ff61fef2b83ad555d6f4c1ebcff6e8a7df89
TrujoDev <andetox@gmail.com>
2024-03-29 15:20:46 -0600

    Cambio de 1 linea

--- Dia 5/Args.py
+++ Dia 5/Args.py
@@ -27,7 +27,6 @@ def suma_cuadrados(*args):
 Crea una función llamada suma_absolutos, que tome un conjunto de argumentos de cualquier extensión, y retorne la suma de sus valores absolutos (es decir, que tome los valores sin signo y los sume, o lo que es lo mismo, los considere a todos -negativos y positivos- como positivos)."""
 
 def suma_absolutos(*args):
-    lista = []
     total = 0
     for arg in args:
         absoluto = abs(arg)
